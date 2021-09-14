8458ee5836674ebfbd324d7183dbabd1e48e007a
Radiaci9 <cvirko1998@mail.ru>
2021-09-14 15:29:31 +0300

    [React-router-v5] - Code split

--- react-router-v5/src/components/App.js
+++ react-router-v5/src/components/App.js
@@ -5,38 +5,43 @@ import {
   Switch
 } from 'react-router-dom';
 
-import FourOhFour from './FourOhFour';
-import Home from './Home';
 import Navbar from './Navbar';
-import Players from './Players';
-import Teams from './Teams';
-import TeamPage from './TeamPage';
-import TeamArticles from './TeamArticles';
+import Loading from './Loading';
+
+const FourOhFour = React.lazy(() => import('./FourOhFour'));
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const TeamArticles = React.lazy(() => import('./TeamArticles'));
 
 export default function App () {
   return (
     <Router>
       <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route path="/players">
-          <Players />
-        </Route>
-        <Route path="/teams">
-          <Teams />
-        </Route>
-        <Route exact path="/:teamName">
-          <TeamPage />
-        </Route>
-        <Route path="/:teamName/articles">
-          <TeamArticles />
-        </Route>
-        <Route path="*">
-          <FourOhFour />
-        </Route>
-      </Switch>
+
+      <React.Suspense fallback={<Loading />}>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/players">
+            <Players />
+          </Route>
+          <Route path="/teams">
+            <Teams />
+          </Route>
+          <Route exact path="/:teamName">
+            <TeamPage />
+          </Route>
+          <Route path="/:teamName/articles">
+            <TeamArticles />
+          </Route>
+          <Route path="*">
+            <FourOhFour />
+          </Route>
+        </Switch>
+      </React.Suspense>
     </Router>
   )
 }
