568fcf9ffbbd2377c5d3aa76ec22d7b86fa4965e
Radiaci9 <cvirko1998@mail.ru>
2022-11-23 00:00:40 +0100

    [React-query - Course Project] - Issue Search

--- react-query/course-project/src/components/IssuesList.jsx
+++ react-query/course-project/src/components/IssuesList.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import useIssuesList from "../queries/useIssuesList";
+import useIssueSearch from "../queries/useIssueSearch";
 import IssueItem from "./IssueItem";
 
 const IssuesList = ({labels, status}) => {
+  const [searchValue, setSearchValue] = useState("");
   const {isLoading, data} = useIssuesList({labels, status});
+  const {fetchStatus, isLoading: isSearchLoading, data: searchedIssues} = useIssueSearch(searchValue);
 
   return (
     <div>
-      <h1>Issues List</h1>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setSearchValue(event.target.elements.search.value)
+        }}
+      >
+        <label htmlFor="search">Search Issues</label>
+        <input
+          type="search"
+          placeholder="Search"
+          name="search"
+          id="search"
+          onChange={(event) => {
+            if (event.target.value.length === 0) {setSearchValue("");}
+          }}
+        />
+      </form>
+      <h2>Issues List</h2>
       {
-        isLoading ? <p>Loading...</p> : (
+        isLoading ? <p>Loading...</p> : fetchStatus === "idle" && isSearchLoading ? (
           <ul className="issues-list">
             {
               data.map((issue) => (
@@ -26,8 +47,33 @@ const IssuesList = ({labels, status}) => {
               ))
             }
           </ul>
-        )
-      }
+        ) : (
+          <>
+            <h2>Search Results</h2>
+            {isSearchLoading ? (
+              <p>Loading...</p>
+            ) : (
+              <>
+                <p>{searchedIssues.count} Results</p>
+                <ul className="issues-list">
+                  {searchedIssues.items.map((issue) => (
+                    <IssueItem
+                      key={issue.id}
+                      title={issue.title}
+                      number={issue.number}
+                      assignee={issue.assignee}
+                      commentCount={issue.comments.length}
+                      createdBy={issue.createdBy}
+                      createdDate={issue.createdDate}
+                      labels={issue.labels}
+                      status={issue.status}
+                    />
+                  ))}
+                </ul>
+              </>
+            )}
+          </>
+        )}
     </div>
   );
 }
---
+++ react-query/course-project/src/queries/useIssueSearch.js
@@ -0,0 +1,13 @@
+
+
+import { useQuery } from "react-query";
+
+const useIssueSearch = (searchValue) => useQuery(
+  ["issues", "search", searchValue],
+  () => fetch(`/api/search/issues?q=${searchValue}`).then(res => res.json()),
+  {
+    enabled: searchValue.length > 1,
+  }
+);
+
+export default useIssueSearch;
